7efbd8460160cfdf08499be04d42a763bacfe080
Dinika Saxena <dinika.saxena@cern.ch>
2018-06-11 16:37:33 +0200

    create redux store and dispatch actions
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

---
+++ redux-basics.js
@@ -0,0 +1,30 @@
+const redux = require('redux');
+//store
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0,
+}
+const rootReducer = (state = initialState, action) => {
+  if(action.type === 'INCREMENT_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    }
+  }
+  if(action.type === 'ADD_TO_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  } 
+  return state;
+}
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+//dispatch action
+store.dispatch({type: 'INCREMENT_COUNTER'});
+store.dispatch({type: 'ADD_TO_COUNTER', value: 5});
+console.log(store.getState());
+//subscription
