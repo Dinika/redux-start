89078953e3fb4be6ac396e4587a57b14b3e2177f
Dinika Saxena <dinika.saxena@cern.ch>
2018-06-27 13:39:06 +0200

    export functionality of remove counter in a seperate function
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- src/store/reducers/resultsReducer.js
+++ src/store/reducers/resultsReducer.js
@@ -5,15 +5,19 @@ const initialState = {
   results: []
 }
 
+const removeCounter = (state, action) => {
+  const updatedResults = state.results.filter( (result) => {
+    return result.id !== action.payload.idToRemove
+  });
+  return updateObject(state, {results: updatedResults})
+}
+
 const resultsReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_COUNTER:
       return updateObject(state, {results: state.results.concat({id: new Date(), value: action.payload.currentCounterValue})})
     case actionTypes.REMOVE_COUNTER:
-      const updatedResults = state.results.filter( (result) => {
-        return result.id !== action.payload.idToRemove
-      });
-      return updateObject(state, {results: updatedResults})
+      return removeCounter(state, action)
     default:
       return state;
   }
